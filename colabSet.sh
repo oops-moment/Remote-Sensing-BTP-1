1e20bce45f5d31f83ac3589202a8df35446f832d
Vanshika-Dhingra <vanshikadhingra1030@gmail.com>
2023-12-18 13:20:15 +0000

    added file for colab set

---
+++ colabSet.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/oops-moment/Remote-Sensing-BTP-1.git
+cd Remote-Sensing-BTP-1/
+git config --global user.email "vanshikadhingra1030@gmail.com"
+git config --global user.name "Vanshika-Dhingra"
+pip install pyrsgis
+pip install matplotlib_scalebar
